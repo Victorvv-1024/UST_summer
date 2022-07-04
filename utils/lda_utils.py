fff1058fe8500c189944dfc7491663a84a4b831e
Victorvv <ZCABHWE@ucl.ac.uk>
2022-07-04 22:02:30 +0100

    finish the lda utils

--- utils/lda_utils.py
+++ utils/lda_utils.py
@@ -74,21 +74,46 @@ def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
 def doc_word_matrix(content, min_df):
     """
     A function to create the document-word matrix to consider words that has occured min_df times,
-    remove built-in english stopwords, cover
+    remove built-in english stopwords, covert all words to lowercase and remove any words that are below length 2
     Args:
-        content (_type_): _description_
-        min_df (_type_): _description_
+        content (list): lemmeatized content
+        min_df (int): minimum required occurences of a word 
 
     Returns:
-        _type_: _description_
+        list: the doc-word matrix
     """
     
     vectorizer = CountVectorizer(analyzer='word',       
-                             min_df=min_df,                        # minimum reqd occurences of a word 
+                             min_df=min_df,                    # minimum reqd occurences of a word 
                              stop_words='english',             # remove stop words
                              lowercase=True,                   # convert all words to lowercase
-                             token_pattern='[a-zA-Z0-9]{3,}',  # num chars > 3
+                             token_pattern='[a-zA-Z0-9]{2,}',  # num chars >= 2
                              # max_features=50000,             # max number of uniq words
                             )
     
     return vectorizer.fit_transform(content)
+
+def fit_lda_model(doc_word_matrix, n_topics=10, max_iter=100, batch_size=256):
+    """
+    A function to fit the LDA model on the Document-Word Matrix
+    Args:
+        doc_word_matrix (list): the document-word matrix
+        n_topics (int, optional): number of allowed topics. Defaults to 10.
+        max_iter (int, optional): max number of learning iterations, epoches. Defaults to 100.
+        batch_size (int, optional):  n docs in each learning iter. Defaults to 256.
+
+    Returns:
+        _type_: _description_
+    """
+    # build the model
+    lda_model = LatentDirichletAllocation(n_topics=n_topics,     # Number of topics
+                                      max_iter=max_iter,         # epochs
+                                      learning_method='online',   
+                                      random_state=100,          
+                                      batch_size=batch_size,   
+                                      evaluate_every = -1,       # compute perplexity every n iters, default: Don't
+                                      n_jobs = -1,               # Use all available CPUs
+                                     )
+    # fit the model
+    history = lda_model.fit_transform(doc_word_matrix)
+    return history
